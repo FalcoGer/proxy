758b07a6c9675c3b5456ada13a8a956a359cbef1
Jacob Tracy <50470232+Xenon-Blu@users.noreply.github.com>
2023-02-09 09:43:26 -0600

    Revised code comments, commented out packet drop

--- proxy/parser.py
+++ proxy/parser.py
@@ -1,6 +1,6 @@
-# struct is used to decode bytes into primitive data types
+# Struct is used to decode bytes into primitive data types.
 import struct
-# queue is used as a thread safe data structure for packets to be sent to the client or server
+# Queue is used as a thread safe data structure for packets to be sent to the client or server.
 import queue
 
 SERVER_QUEUE = queue.SimpleQueue()
@@ -9,11 +9,9 @@ CLIENT_QUEUE = queue.SimpleQueue()
 def parse(data, port, origin):
     sign = '->' if origin == 'client' else '<-'
     print(f"c{sign}s: {data}")
-    # do interesting stuff with the data!
-    # don't append to queue to just drop the package
-    if data.find(b'fuck') >= 0:
-        print("Dropped")
-        return
+    #if data.find(b'insert packet here') >= 0:
+    #   print("Dropped")
+    #   return
     if (origin == 'client'):
         SERVER_QUEUE.put(data)
     elif (origin == 'server'):
